a273c6d22d67c4ad8ccabb9a20e982bf9baa7a84
Alessandro Nocella <alessandro.nocella@phone4biz.it>
2016-04-01 09:34:00 +0200

    aggiunta pagina rif3

---
+++ pages/rif3.php
@@ -0,0 +1,36 @@
+	<div id="banner" class="riferimenti">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h1>BLOCCO STIPENDI STATALI: AL VIA L'AZIONE COLLETTIVA DI CODACONS</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="row spacer-50 hidden-xs"></div>
+	<div class="row spacer-20 hidden-xs"></div>
+	
+	<div id="main">
+		<div class="container">
+			<div class="row"> <!-- main blocco 1 -->
+				<div class="col-sm-6 riferimenti" id="main_left">
+					<h3>Risarcimento danni da atto lecito della P.A.</h3><br><br>									
+					<p class="small text-justify">
+						<strong>Il principio.</strong> Il problema della responsabilità da atto lecito dell’Amministrazione (anche detto problema dell’indennità e del principio indennitario nel diritto amministrativo) concerne le ipotesi in cui quest’ultima, nel legittimo svolgimento della propria attività e, dunque, nel perseguimento di interessi generali, arrechi danni a singoli individui o collettività che, a fronte del generale beneficio derivante dall’agire pubblico, risultino gravati dei relativi oneri. <br><br>
+						<strong>La regola generale.</strong> Che la responsabilità da atto lecito dello Stato costituisca una regola generale è stato sostenuto dalla dottrina italiana tra la fine dell’Ottocento e l’inizio del Novecento. L’idea, che incontrava i percorsi di una parte della dottrina europea, in particolar modo tedesca, era supportata da un’esigenza pratica di effettività di tutela: e cioè dalla difficoltà di applicare, in concreto, la pur affermata responsabilità dell’Amministrazione, muovendo dalla disciplina civilistica del fatto illecito, fondata sulla colpa: l’impossibilità di sindacare la discrezionalità (anche solo tecnica) dell’Amministrazione si traduceva infatti nell’impossibilità di accertarne la colpa. In risposta a questo problema la dottrina veniva tracciando le linee di una generale responsabilità dell’Amministrazione avente una base diversa dalla colpa ed estesa anche agli atti leciti73. La tesi implicava che ad ogni sacrificio speciale imposto all’individuo.<br><br>
+						<strong>La natura della responsabilità della P.A.</strong> In base a principi di giustizia distributiva e riparativa e in virtù della sopravvivenza del diritto nel conflitto con il potere pubblico, l’ente pubblico che, facendo uso di un suo potere, impone a un diritto individuale un particolare sacrificio, che non è compreso nei limiti normali di esso, deve risarcirlo con una proporzionata indennità. Di qui l’idea di una responsabilità (anche da atto lecito) non limitata ai casi contemplati dalla legge. La responsabilità dell’Amministrazione si è così articolata, nel sistema italiano, in un doppio binario: un primo, riguardante la responsabilità dell’Amministrazione per fatto illecito, fondata sulla colpa e disciplinata dalle generali previsioni del codice civile; un secondo, relativo alle eccezionali e disomogenee ipotesi di responsabilità per fatto lecito contemplate dalla legge e privo di una regola di carattere generale. In questo quadro, la dottrina ha più volte tentato di affermare un principio generale di responsabilità da atto lecito. Il percorso per lo più seguito è stato quello di desumerlo dalle diverse figure tipiche previste dalla legge. In particolare il riferimento è stato alla disciplina dell’espropriazione per pubblica utilità e a quella concernente la riparazione dei danni derivanti dall’esecuzione di opere pubbliche a proprietari non espropriati, facendo leva su interpretazioni estensive di tali norme.<br><br>
+						<strong>La riparazione del danno.</strong> La principale conseguenza pratica della questione concernente la natura giuridica della responsabilità da atto lecito attiene al regime giuridico del dovere riparatorio e, in particolare, alla quantificazione del ristoro dovuto. Il riferimento ad un dovere indennitario di natura pubblicistica, fondato sul principio solidaristico, ha consentito di giustificare la discrezionalità del legislatore nell’individuazione dell’ammontare degli indennizzi e di sganciarli dalla necessaria correlazione con il danno. La Corte Costituzionale ha così riconosciuto che il legislatore può discrezionalmente determinare l’entità della misura riparatoria (indennità) nei limiti della non irrisorietà. Solidarietà vale così discrezionalità del legislatore nel definire il contenuto della garanzia e, in particolare, l’ampiezza e la misura della garanzia. Si è, per tale via, riconosciuta la legittimità di indennizzi non idonei a riparare l’intero pregiudizio subito, e ciò è avvenuto in materia di espropriazioni, vaccinazioni obbligatorie, ingiusta detenzione. In tutti questi casi si è consentito di bilanciare la garanzia di diritti fondamentali con altri interessi, tra cui il contenimento della spesa pubblica. Come si è visto, con riferimento alla proprietà e, più in generale, alla sfera patrimoniale, quest’ordine argomentativo è stato superato grazie all’intervento della Corte europea dei diritti dell’uomo che ha affermato la necessaria correlazione tra misura riparatoria e pregiudizio effettivamente subito. 
+					</p>
+				</div>
+				<div class="col-sm-6">
+					<?php require_once ('templates/aderisci.php'); ?>
+				</div>
+			</div><!-- /main blocco 1 -->			
+		</div>	
+		
+		<div class="row spacer-50"></div>
+		<div class="row spacer-50"></div>
+		<div class="row spacer-20"></div>
+		<div class="row spacer-20"></div>
+	</div>	
