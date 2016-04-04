062dac426fa0016ef48b69451f10c0980d95da3a
Alessandro Nocella <alessandro.nocella@phone4biz.it>
2016-04-04 16:32:01 +0200

    preparato per meta tags; sistemato define index

--- templates/open.php
+++ templates/open.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <title>Blocco Stipendi CODACONS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="title" content="<?php echo $metat; ?>">
+	<meta name="description" content="<?php echo $metad; ?>">
 
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
