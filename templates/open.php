339e8dc44937a16647de7781bbcea22cf8ab7f8a
Alessandro Nocella <alessandro.nocella@phone4biz.it>
2016-04-05 09:31:02 +0200

    sistemato css

--- templates/open.php
+++ templates/open.php
@@ -34,8 +34,8 @@
 	<link href='https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link href="<?php echo HOME;?>css/jquery-eu-cookie-law-popup.css" rel="stylesheet">
-	<link href="<?php echo HOME;?>css/work/custom.css" rel="stylesheet">
-	<!-- <link href="<?php echo HOME;?>css/style.min.css" rel="stylesheet"> -->
+	<!--<link href="<?php echo HOME;?>css/work/custom.css" rel="stylesheet">-->
+	<link href="<?php echo HOME;?>css/style.min.css" rel="stylesheet">
 </head>
 
 <body class="eupopup">
