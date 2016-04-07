3ff14cd77d1bc84e3fe7ae427ff039240f5595f9
Alessandro Nocella <alessandro.nocella@phone4biz.it>
2016-04-07 12:04:08 +0200

    aggiunta 404

---
+++ 404/index.php
@@ -0,0 +1,14 @@
+<?php
+
+	define ('HOME', 'http://bloccostipendi.codacons.it/');	
+	
+	$metat="Blocco Stipendi Statali | campagna promossa da Codacons";
+	$metad="Il Codacons avvia una campagna in favore dei dipendenti pubblici contro il blocco degli stipendi per chiedere il rinnovo dei contratti ed il risarcimento";
+
+	require_once('../templates/open.php');
+	require_once('../templates/header.php');
+	require_once('../pages/404.php');
+	require_once('../templates/footer.php');
+	require_once('../templates/close.php');
+	
+?>
---
+++ pages/404.php
@@ -0,0 +1,30 @@
+	<div id="banner" class="privacy">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h1>BLOCCO STIPENDI STATALI: AL VIA L'AZIONE COLLETTIVA DI CODACONS</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="row spacer-50 hidden-xs"></div>
+	<div class="row spacer-20 hidden-xs"></div>
+	
+	<div id="main">
+		<div class="container" id="nontrovata">
+			<div class="row"> <!-- main blocco 1 -->				
+				<div class="col-xs-12 text-center">
+					<i class="fa fa-chain-broken fa-404"></i>
+					<span class="quattro">404</span>
+					<span class="notfound">PAGINA NON TROVATA</span>
+					<span><a href="<?php echo HOME;?>">Torna all'homepage</a></span>
+				</div>
+			</div><!-- /main blocco 1 -->			
+		</div>	
+		
+		<div class="row spacer-50"></div>
+		<div class="row spacer-50"></div>
+		<div class="row spacer-20"></div>
+		<div class="row spacer-20"></div>
+	</div>	
