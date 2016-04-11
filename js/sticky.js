f3a5d06f8bcca62a9e48c1424a28fd5100a1ca1a
Alessandro Nocella <alessandro.nocella@phone4biz.it>
2016-04-11 09:58:45 +0200

    sistemando bug sticky

---
+++ js/sticky.js
@@ -0,0 +1,21 @@
+	
+function stick(element,t,b) {
+	$.lockfixed(element,
+		{offset: {top: t, bottom: b}}
+	);
+}
+
+
+$(function() {
+	var w=$(window).width();
+	if (w>=768){
+		stick("#sticky_aderisci",10,100);
+	}
+});
+
+$(window).scroll(function() {
+	var w=$(window).width();
+	if (w>=768){
+		stick("#sticky_aderisci",10,100);
+	}
+});
--- templates/close.php
+++ templates/close.php
@@ -1,17 +1,11 @@
 	
 	<script src="https://code.jquery.com/jquery-2.2.2.min.js" integrity="sha256-36cp2Co+/62rEAAYHLmRCPIych47CvdM+uTBJwSzWjI=" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>	
-	<script src="<?php echo HOME; ?>js/jquery.lockfixed.min.js"></script>	
+	<script src="<?php echo HOME; ?>js/jquery.lockfixed.js"></script>	
 	<script src="<?php echo HOME; ?>js/jquery-eu-cookie-law-popup.js"></script>	
 	<!--<script src="<?php echo HOME; ?>js/func.min.js"></script>-->
 	<script src="<?php echo HOME; ?>js/scrolltop.js"></script>		
-	<script src="<?php echo HOME; ?>js/jquery.sticky.js"></script>			
-	
-	<script>
-	  $(document).ready(function(){
-		$("#sticky_aderisci").sticky({topSpacing:10});
-	  });
-	</script>
+	<script src="<?php echo HOME; ?>js/sticky.js"></script>		
 	
 </body>
 </html>
