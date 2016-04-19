f26443a37c921e3371dc5dbf70943e41f5232895
Alessandro Nocella <alessandro.nocella@phone4biz.it>
2016-04-19 10:17:54 +0200

    riferimenti normativi diventa presupposti anche nei link

--- templates/header.php
+++ templates/header.php
@@ -20,18 +20,18 @@
 				  <a href="#" class="dropdown-toggle <?php echo $active1; ?>" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">I PRESUPPOSTI &nbsp;<i class="fa fa-angle-down"></i></a>
 				  <ul class="dropdown-menu">
 					<li class="color hidden-xs text-right" id="presupposti"><a href="#">I PRESUPPOSTI &nbsp;<i class="fa fa-angle-down"></i></a></li>
-					<li><a href="<?php echo HOME;?>riferimenti-normativi/sentenza-corte-costituzionale-178_2015">Sentenza C.C. n.178/2015</a></li>
-					<li><a href="<?php echo HOME;?>riferimenti-normativi/legge-adeguamenti-stipendi-pubblica-amministrazione">Legge adeguamenti stipendi P.A.</a></li>					
-					<li><a href="<?php echo HOME;?>riferimenti-normativi/risarcimento-danni">Risarcimento danni</a></li>					
+					<li><a href="<?php echo HOME;?>presupposti/sentenza-corte-costituzionale-178_2015">Sentenza C.C. n.178/2015</a></li>
+					<li><a href="<?php echo HOME;?>presupposti/legge-adeguamenti-stipendi-pubblica-amministrazione">Legge adeguamenti stipendi P.A.</a></li>					
+					<li><a href="<?php echo HOME;?>presupposti/risarcimento-danni">Risarcimento danni</a></li>					
 				  </ul>
 				</li>
 				<li class="dropdown clearfix visible-xs-block">
 				  <a href="#" class="dropdown-toggle <?php echo $active1; ?>">I PRESUPPOSTI &nbsp;<i class="fa fa-angle-down"></i></a>
 				  <ul class="dropdown-menu" id="menu_small">
 					<li class="color hidden-xs text-right" id="presupposti"><a href="#">I PRESUPPOSTI &nbsp;<i class="fa fa-angle-down"></i></a></li>
-					<li><a href="<?php echo HOME;?>riferimenti-normativi/sentenza-corte-costituzionale-178_2015">Sentenza C.C. n.178/2015</a></li>
-					<li><a href="<?php echo HOME;?>riferimenti-normativi/legge-adeguamenti-stipendi-pubblica-amministrazione">Legge adeguamenti stipendi</a></li>					
-					<li><a href="<?php echo HOME;?>riferimenti-normativi/risarcimento-danni">Risarcimento danni</a></li>					
+					<li><a href="<?php echo HOME;?>presupposti/sentenza-corte-costituzionale-178_2015">Sentenza C.C. n.178/2015</a></li>
+					<li><a href="<?php echo HOME;?>presupposti/legge-adeguamenti-stipendi-pubblica-amministrazione">Legge adeguamenti stipendi</a></li>					
+					<li><a href="<?php echo HOME;?>presupposti/risarcimento-danni">Risarcimento danni</a></li>					
 				  </ul>
 				</li>
 				<li><a href="<?php echo HOME;?>categorie-interessate" class="<?php echo $active2; ?>">CATEGORIE INTERESSATE</a></li>
