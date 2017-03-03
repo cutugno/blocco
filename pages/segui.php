55b0d9a82f938aa1d327e4a13828de321cc9c518
Alessandro Nocella <alessandro.nocella@phone4biz.it>
2017-03-03 17:30:20 +0100

    sistemato css form segui

--- pages/segui.php
+++ pages/segui.php
@@ -14,7 +14,7 @@
 	<div id="main">
 		<div class="container">
 			<div class="row"> <!-- main blocco 1 -->
-				<div class="col-xs-12 aderisci" id="main_left">
+				<div class="col-xs-12 segui" id="main_left">
 					<h3>Segui l'iniziativa</h3>
 					<p class="small">
 					Compilando il questionario chiede di essere informato dall'Associazione riguardo a tutti gli sviluppi dell'iniziativa intrapresa in favore dei dipendenti pubblici.
@@ -22,7 +22,7 @@
 					<p class="small">(I campi contrassegnati con l'asterisco <span style="color:red">*</span> sono obbligatori)<br><br>
 					Dopo aver compilato il modulo online e cliccato sul pulsante "INVIA LE INFORMAZIONI" torna sempre sopra ad inizio pagina per confermare i dati e l'invio della mail.
 					</p>
-					<iframe id="form_aderisci" src="http://www.termilcons.net/index.php?pagina=page_publicForm&idForm=510&css=1&access=ok"></iframe>
+					<iframe id="form_segui" src="http://www.termilcons.net/index.php?pagina=page_publicForm&idForm=510&css=1&access=ok"></iframe>
 					<p class="small">
 					<strong>Dopo aver compilato il modulo online e cliccato sul pulsante "INVIA LE INFORMAZIONI" torna sempre all'inizio di questa pagina per confermare i dati e l'invio della mail.</strong><br><br>
 					Cliccando su "Invia le informazioni" autorizzi l'ente Ter.Mil.Cons., delegato da CODACONS, al trattamento dei dati personali ex D. Lgs. n. 196/2003 e ss. mm. (leggi <a href="<?php echo HOME; ?>privacy">qui</a> le condizioni generali del trattamento)
